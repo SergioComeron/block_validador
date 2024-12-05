5f04cac3b0e378fa680247cbddd8a206e06bc716
SergioComeron <sergiocomeron@icloud.com>
2024-12-05 10:58:22 +0100

    Inicialización del bloque validador

---
+++ block_validador.php
@@ -0,0 +1,21 @@
+<?php
+class block_validador extends block_base {
+    public function init() {
+        $this->title = get_string('pluginname', 'block_hello_course');
+    }
+
+    public function get_content() {
+        if ($this->content !== null) {
+            return $this->content;
+        }
+
+        global $COURSE;
+
+        // Contenido del bloque
+        $this->content = new stdClass();
+        $this->content->text = get_string('message', 'block_validador', $COURSE->shortname);
+        $this->content->footer = '';
+
+        return $this->content;
+    }
+}
---
+++ lang/en/block_validador.php
@@ -0,0 +1,4 @@
+<?php
+// General strings
+$string['pluginname'] = 'Hello Validador';
+$string['message'] = 'Hello, you are in the course: {$a}.';
